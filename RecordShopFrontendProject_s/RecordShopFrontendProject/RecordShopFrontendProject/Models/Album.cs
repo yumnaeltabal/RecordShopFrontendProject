e4ec7a80dd4ac1edb3f1d391abd4e7079282a092
yumna <yumnaeltaba@gmail.com>
2026-05-19 10:53:15 +0100

    created albums summary and albums summary list with a hardcoded list of albums

---
+++ RecordShopFrontendProject_s/RecordShopFrontendProject/RecordShopFrontendProject/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿namespace RecordShopFrontendProject.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+
+        public Album(string title, string artist, int releaseYear, string genre)
+        {
+            Title = title;
+            Artist = artist;
+            ReleaseYear = releaseYear;
+            Genre = genre;
+        }
+    }
+}
---
+++ RecordShopFrontendProject_s/RecordShopFrontendProject/RecordShopFrontendProject/Models/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace RecordShopFrontendProject.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
